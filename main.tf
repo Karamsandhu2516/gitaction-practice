718eb645946734fc184ab819bf7467e3f787fe75
Karamjit Kaur <karamsandhu2516@gmail.com>
2026-05-06 19:11:33 -0400

    fmt fix

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_s3_bucket" "practice_bucket" {
   # Change this to something unique like your name + date
-  bucket = "karamjit-sandhu-practice-2026-v1" 
+  bucket = "karamjit-sandhu-practice-2026-v1"
 
   tags = {
-      Name        = "Practice"
-      Environment = "DevOps-Learning"
+    Name        = "Practice"
+    Environment = "DevOps-Learning"
   }
 }
